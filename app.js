f8a075176490af29ff85b01bf12d64c7c2cc3c69
Marko Martinović <mmarti10@racunarstvo.hr>
2023-03-06 13:29:18 +0100

    Port change

--- app.js
+++ app.js
@@ -111,12 +111,12 @@ app.get("/welcome", auth, (req, res) => {
   res.status(200).send("Welcome 🙌 ");
 });
 
-// const port = process.env.PORT || API_PORT;
+const port = process.env.PORT || 3030;
 
 // // your code
 
-// app.listen(port, () => {
-//   console.log(`server started on port ${port}`);
-// });
+app.listen(process.env.PORT, () => {
+  console.log(`server started on port ${process.env.PORT}`);
+});
 
 module.exports = app;
